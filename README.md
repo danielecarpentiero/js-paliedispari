aa8a8447c557c2c3051161044a8958e0ebf403d9
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-14 16:10:05 +0100

    feat: readme & first attempt

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# js-paliedispari
+# Palindroma
+
+# Pali e dispari
+
+1. chiedo all'utente di compiere una scelta fra pari o dispari.
+   - se l'utente sceglie pari, al computer verrà assegnato dispari.
+   - altrimenti, il contrario.
+2. chiedo all'utente di scegliere un numero compreso fra 1 e 5.
+3. genero una funzione per generare un numero compreso fra 1 e 5.
+4. sommo i due numeri.
+   - se la somma è dispari, chi ha scelto in origine dispari vince, mentre pari perde
+   - altrimenti, pari vince.
---
+++ paliedispari/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const playerChoice = prompt("scegli fra pari o dispari");
+
+let computerChoice = "";
+
+function computerNumber() {
+  const computerRandom = Math.floor(Math.random() * 5 + 1);
+  return computerRandom;
+}
+console.log(computerNumber);
+
+if (playerChoice === "pari") {
+  computerChoice = "dispari";
+  console.log(`La scelta del giocatore è ${playerChoice}.`);
+  console.log(`La scelta del computer è ${computerChoice}.`);
+} else if (playerChoice === "dispari") {
+  computerChoice = "pari";
+  console.log(`La scelta del giocatore è ${playerChoice}.`);
+  console.log(`La scelta del computer è ${computerChoice}.`);
+} else {
+  console.log("hai inserito un valore non valido");
+}
+
+const sum = playerNumber + computerNumber;
+
+function checkSum(playerNumber, computerNumber) {
+  if ((playerNumber + computerNumber) % 2 === 0) {
+    console.log("umano vince!");
+  } else {
+    console.log("computer vince!");
+  }
+}
+
+const playerNumber = +prompt("scegli un numero compreso fra 1 e 5");
+if (playerNumber >= 1 && playerNumber <= 5) {
+  console.log(checkSum);
+}
