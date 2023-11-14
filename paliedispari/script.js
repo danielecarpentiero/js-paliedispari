19e0e56e3351ecdf286f584694a5f4c8b2573598
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-14 17:55:45 +0100

    fix: script

--- paliedispari/script.js
+++ paliedispari/script.js
@@ -1,37 +1,48 @@
 "use strict";
 
-const playerChoice = prompt("scegli fra pari o dispari");
+let playerChoice = prompt("scegli fra pari o dispari");
+while (playerChoice !== "pari" && playerChoice !== "dispari") {
+  playerChoice = prompt("Valore errato, scegli fra pari o dispari");
+}
 
-let computerChoice = "";
+if (playerChoice === "pari") {
+  console.log("il giocatore ha scelto pari, il computer è quindi dispari.");
+} else {
+  console.log("il giocatore ha scelto dispari, il computer è quindi pari.");
+}
+
+let playerNumber = Number(prompt("scegli un numero compreso fra 1 e 5"));
+while (playerNumber <= 1 || playerNumber >= 5 || isNaN(playerNumber)) {
+  playerNumber = Number(
+    prompt("Inserisci un numero valido compreso fra 1 e 5")
+  );
+}
+console.log(`il giocatore ha scelto ${playerNumber}`);
+
+let computerChoice = computerNumber();
+console.log(`il computer ha scelto ${computerChoice}`);
+
+const totSum = playerNumber + computerChoice;
+console.log(`la somma dei due numeri è ${totSum}`);
+
+const oddEvenRes = checkSum(totSum);
+console.log(`siccome è ${oddEvenRes}, allora`);
+
+if (playerChoice === oddEvenRes) {
+  console.log("il giocatore vince!");
+} else {
+  console.log("il computer vince!");
+}
 
 function computerNumber() {
   const computerRandom = Math.floor(Math.random() * 5 + 1);
   return computerRandom;
 }
-console.log(computerNumber);
 
-if (playerChoice === "pari") {
-  computerChoice = "dispari";
-  console.log(`La scelta del giocatore è ${playerChoice}.`);
-  console.log(`La scelta del computer è ${computerChoice}.`);
-} else if (playerChoice === "dispari") {
-  computerChoice = "pari";
-  console.log(`La scelta del giocatore è ${playerChoice}.`);
-  console.log(`La scelta del computer è ${computerChoice}.`);
-} else {
-  console.log("hai inserito un valore non valido");
-}
-
-const playerNumber = prompt("scegli un numero compreso fra 1 e 5");
-
-function checkSum(playerNumber, computerNumber) {
-  if ((playerNumber + computerNumber) % 2 === 0) {
-    console.log("umano vince!");
+function checkSum(number) {
+  if (number % 2 === 0) {
+    return "pari";
   } else {
-    console.log("computer vince!");
+    return "dispari";
   }
-}
-
-if (playerNumber >= 1 && playerNumber <= 5) {
-  console.log(checkSum);
 }
